024bc6a52f950c66e171e6a2248e2ac35bed0896
p1geon <p1geon@spyro.fritz.box>
2026-02-10 22:10:14 +0100

    remove radix offset error

--- build_db.py
+++ build_db.py
@@ -21,6 +21,20 @@ class SharedMem:
     patterns_done:int = 0
     done:bool = False
 
+def progress(mem:SharedMem):
+    time_start = perf_counter()
+    patterns_total = FIRST_DIGITS_AMOUNT * mem.size * mem.files_total
+
+    while not mem.done:
+        sleep(0.2)
+        if mem.patterns_done == 0: continue
+        time_elapsed = perf_counter() - time_start
+        time_elapsed_f = format_time(time_elapsed)
+        speed_pattern = mem.patterns_done / time_elapsed
+        speed_pattern_f = format_size(speed_pattern, "/s")
+        eta = format_time(patterns_total / speed_pattern - time_elapsed)
+        print(" "*100+"\r"+f"elapsed:{time_elapsed_f}\t eta:{eta}\t patterns:{speed_pattern_f}\t files:{mem.files_done}/{mem.files_total}", end="          \r")
+
 def get_patterns(chunk:str|bytes, max_length:int=10, offset:int=0):
     positions = chain.from_iterable((x+offset,)*max_length for x in range(len(chunk)-max_length))
     patterns = []
@@ -43,40 +57,24 @@ def _build(mem:SharedMem):
             string_datatype = "TEXT" if num.format == "txt" else "BLOB"
             cursor.execute(f"CREATE TABLE IF NOT EXISTS {num.table_name} (string {string_datatype} PRIMARY KEY, position INTEGER)")
 
-        for chunk, offset in ((num.first_digits[x:x+patterns_per_insert],x+1) for x in range(num.radix_pos-1, len(num.first_digits), patterns_per_insert)):
-            patterns = get_patterns(chunk, size, offset)
+        for startpos in range(0, FIRST_DIGITS_AMOUNT, patterns_per_insert):
+            chunk = num.first_digits[startpos:startpos+patterns_per_insert]
+            patterns = get_patterns(chunk, size, startpos+1)
             with mem.db_lock:
                 cursor.executemany(f"INSERT OR IGNORE INTO {num.table_name} VALUES (?, ?)", patterns)
                 conn.commit()
             mem.patterns_done += size * patterns_per_insert
         mem.files_done += 1
 
-def progress(mem:SharedMem):
-    time_start = perf_counter()
-    patterns_total = FIRST_DIGITS_AMOUNT * mem.size * mem.files_total
-
-    while not mem.done:
-        sleep(0.3)
-        if mem.patterns_done == 0: continue
-        time_elapsed = perf_counter() - time_start
-        time_elapsed_f = format_time(time_elapsed)
-        speed_pattern = mem.patterns_done / time_elapsed
-        speed_pattern_f = format_size(speed_pattern, "/s")
-        eta = format_time(patterns_total / speed_pattern - time_elapsed)
-        print(" "*70+"\r"+f"elapsed:{time_elapsed_f}\t eta:{eta}\t patterns:{speed_pattern_f}\t files:{mem.files_done}/{mem.files_total}", end="          \r")
-
 def build(size:int, num_workers:int=6):
     time_start = perf_counter()
 
     mem = SharedMem()
     mem.size = size
-    """
+
     for num in set(get_all()):
         mem.files_total += 1
         mem.file_queue.put(num)
-    """
-    mem.files_total = 1
-    mem.file_queue.put(get_one("pi",10,"txt"))
 
     prog = Thread(target=progress, args=(mem, ))
 
@@ -99,4 +97,4 @@ def build(size:int, num_workers:int=6):
 
 if __name__ == "__main__":
     SQLITE_PATH.unlink(True)
-    build(6)
+    build(10)
