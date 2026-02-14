10daa482a64bef0188a2c251697ec7e227dc280b
p1geon <p1geon@spyro.fritz.box>
2026-02-14 19:25:25 +0100

    add settings.json and add intparts to CONST_TABLE

--- scripts/const.py
+++ scripts/const.py
@@ -1,29 +1,24 @@
 import os
+import json
 from pathlib import Path
 
-NUM_DIR = Path("/home/p1geon/bignum/")
+SETTINGS_PATH = Path("settings.json")
+settings = json.load(SETTINGS_PATH.open())
+NUM_DIR = Path(settings["NUM_DIR"])
+SQLITE_PATH = Path(settings["SQLITE_PATH"])
+NUMS_PER_INSERT = int(settings["NUMS_PER_INSERT"])
 CHUNK_SIZE = 8*2**20
 FIRST_DIGITS_AMOUNT = 1_000_000
 PAGE_SIZE = os.sysconf('SC_PAGE_SIZE')
-SQLITE_PATH = Path("irranalyze.sqlite")
 IDENTIFY_TABLE_NAME = "identify"
 CONST_TABLE = {
-    "41421":"sqrt2",
-    "73205":"sqrt3",
-    "23606":"sqrt5",
-    "16227":"sqrt10",
-    "61803":"phi",
-    "71828":"e",
-    "14159":"pi",
-    "69314":"ln2",
-    "09861":"ln3",
-    "38629":"ln4",
-    "60943":"ln5",
-    "30258":"ln10",
-    "24411":"lemniscate",
-    "20205":"zeta3",
-    "62560":"gamma1/4",
-    "91596":"catalan",
-    "57721":"gamma",
+    "1.41421":"sqrt(2)",
+    "1.61803":"phi",
+    "2.71828":"e",
+    "3.14159":"pi",
+    "5.24411":"lemniscate",
+    "0.69314":"ln(2)",
+    "1.20205":"apery's constant",
+    "0.91596":"catalan",
+    "0.57721":"euler mascheroni",
 }
-
