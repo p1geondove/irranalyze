8aa86e95c47319fd03e8e0ecb230c5741f259600
p1geondove <leo.peil@web.de>
2026-03-01 02:42:10 +0100

    adding init/sanity check

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,15 @@
+# __init__.py - init file to scripts module
+
+def sanity():
+    from .var import Paths, Sizes
+    from pathlib import Path
+    if Paths.num_dir == Path("/path/to/numbers/"):
+        print(f"WARN: NUM_DIR ({Paths.num_dir}) is not set up properly, run setup.py")
+    if Sizes.max_processes == -1:
+        print(f"WARN: MAX_PROCESSES ({Sizes.max_processes}) is not set up properly, run setup.py")
+
+sanity()
+
 from .bignum import BigNum, get_all, get_one
-from .search import search
 from .convert import num_to_txt, txt_to_num, txt_to_num_all, alnum_to_num, base_convert
+from .var import Sizes, Switches, Paths
